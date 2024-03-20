52bb4c94e4b6381afb467f2c4033600781ec39b9
Passam <119688260+Passam02@users.noreply.github.com>
2024-03-20 20:54:04 +0100

    Create CRUD treatments in patients edit forms

--- laravel-vet/app/Models/Treatment.php
+++ laravel-vet/app/Models/Treatment.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Treatment extends Model
 {
     use HasFactory;
+    protected $casts = [
+        'price' => MoneyCast::class,
+    ];
     public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);

38b9f4734bf3312c4a37b9689854eada833de596
Passam <119688260+Passam02@users.noreply.github.com>
2024-03-27 17:54:09 +0100

    Change colors on footer

--- laravel-vet/resources/views/livewire/index.blade.php
+++ laravel-vet/resources/views/livewire/index.blade.php
@@ -163,59 +163,59 @@
                 </div>
             </div>
         </div>
-        <footer class="relative pt-8 pb-6" style="background-color: #f5f0dc">
-  <div class="container mx-auto px-4">
-    <div class="flex flex-wrap text-left lg:text-left">
-      <div class="w-full lg:w-6/12 px-4">
-        <h4 class="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
-        <h5 class="text-lg mt-0 mb-2 text-blueGray-600">
-          Find us on any of these platforms.
-        </h5>
-        <div class="mt-6 lg:mb-0 mb-6">
-          <button class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-twitter"></i></button><button class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-facebook-square"></i></button><button class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-instagram"></i></button>
-          </button>
-        </div>
-      </div>
-      <div class="w-full lg:w-6/12 px-4">
-        <div class="flex flex-wrap items-top mb-6">
-          <div class="w-full lg:w-4/12 px-4 ml-auto">
-            <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
-            <ul class="list-unstyled">
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">About Us</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">Blog</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">Help Animals</a>
-              </li>
-            </ul>
+        <footer class="relative pt-8 pb-6 bg-gray-900">
+          <div class="container mx-auto px-4">
+            <div class="flex flex-wrap text-left lg:text-left">
+              <div class="w-full lg:w-6/12 px-4">
+                <h4 class="text-3xl fonat-semibold text-white">Let's keep in touch!</h4>
+                <h5 class="text-lg mt-0 mb-2 text-white">
+                  Find us on any of these platforms.
+                </h5>
+                <div class="mt-6 lg:mb-0 mb-6">
+                  <button class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                    <i class="fab fa-twitter"></i></button><button class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                    <i class="fab fa-facebook-square"></i></button><button class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                    <i class="fab fa-instagram"></i></button>
+                  </button>
+                </div>
+              </div>
+              <div class="w-full lg:w-6/12 px-4">
+                <div class="flex flex-wrap items-top mb-6">
+                  <div class="w-full lg:w-4/12 px-4 ml-auto">
+                    <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
+                    <ul class="list-unstyled">
+                      <li>
+                        <a class="text-white hover:text-Gray-800 font-semibold block pb-2 text-sm">About Us</a>
+                      </li>
+                      <li>
+                        <a class="text-white hover:text-Gray-800 font-semibold block pb-2 text-sm">Blog</a>
+                      </li>
+                      <li>
+                        <a class="text-white hover:text-Gray-800 font-semibold block pb-2 text-sm">Help Animals</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="w-full lg:w-4/12 px-4">
+                    <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
+                    <ul class="list-unstyled">
+                      <li>
+                        <a class="text-white hover:text-Gray-800 font-semibold block pb-2 text-sm">Diplomas</a>
+                      </li>
+                      <li>
+                        <a class="text-white hover:text-Gray-800 font-semibold block pb-2 text-sm">Terms &amp; Conditions</a>
+                      </li>
+                      <li>
+                        <a class="text-white hover:text-Gray-800 font-semibold block pb-2 text-sm">Privacy Policy</a>
+                      </li>
+                      <li>
+                        <a class="text-white hover:text-Gray-800 font-semibold block pb-2 text-sm">Contact Us</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="w-full lg:w-4/12 px-4">
-            <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
-            <ul class="list-unstyled">
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">Diplomas</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">Terms &amp; Conditions</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">Privacy Policy</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
+        </footer>
     </body>
 </html>
